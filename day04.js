0ebf16071017cb31dff4308c1d1c53109206816c
Markku Marjoneva <markku.marjoneva@gmail.com>
2017-12-04 22:12:45 +0200

    Day04 part 2

--- day04.js
+++ day04.js
@@ -30,3 +30,28 @@ function filter1(phrase) {
 
 //console.log('Test1', test1.filter(filter1).length)
 console.log('Part1', input.filter(filter1).length)
+
+/*
+--- Part Two ---
+
+For added security, yet another system policy has been put in place. Now, a valid passphrase must contain no two words that are anagrams of each other - that is, a passphrase is invalid if any word's letters can be rearranged to form any other word in the passphrase.
+
+For example:
+
+abcde fghij is a valid passphrase.
+abcde xyz ecdab is not valid - the letters from the third word can be rearranged to form the first word.
+a ab abc abd abf abj is a valid passphrase, because all letters need to be used when forming another word.
+iiii oiii ooii oooi oooo is valid.
+oiii ioii iioi iiio is not valid - any of these words can be rearranged to form any other word.
+Under this new system policy, how many passphrases are valid?
+*/
+
+function filter2(phrase) {
+  const words = phrase.split(/\s+/)
+  return !pairs(words)
+    .filter(([a,b]) => a.length == b.length)
+    .map(pair => pair.map(word => word.split('').sort().join('')))
+    .find(([a,b]) => a == b)
+}
+
+console.log('Part2', input.filter(filter2).length)
